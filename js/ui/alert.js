399c5da02fd6db29c272e6ad443dd383d2e16b4b
j113203 <j113203@gmail.com>
2016-02-25 17:24:20 +0800

    //

---
+++ js/ui/alert.js
@@ -0,0 +1,12 @@
+function random(min,max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+(function(proxied) {
+  window.alert = function(message,callback) {
+	var ui_alert = new ui("alert_"+Math.floor(Math.random()*(1e6)));
+	var length = message.length/2+"em" || "10em";
+	ui_alert.render("<i class='fa fa-exclamation-triangle'></i>","","2em",length,"",message,"top:100;left:100;");
+    ui_alert.show("middle middle");
+	ui_alert.error(message,callback);
+  };
+})(window.alert);
